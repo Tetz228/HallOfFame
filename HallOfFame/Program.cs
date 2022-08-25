0c1f48f32e6bdf96ad197337e2ca22a4f485c60f
TETZ\tetz2 <tetz2281337@gmail.com>
2022-08-25 23:24:59 +0700

    Добавил библиотеки Entity и Swagger. Подключил и настроил Swagger.

--- HallOfFame/Program.cs
+++ HallOfFame/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+//Настройка сервисов.
+
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "HallOfFame", Version = "v1"}); });
+builder.Services.AddControllers();
+
 var app = builder.Build();
+//Настройка приложения.
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("v1/swagger.json", "hof-api V1"); });
+}
 
+app.UseRouting();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 app.Run();
